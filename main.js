2ae9fd8c24abbb788f68b2dbd01a83b02b6b2385
jhhyf <30581746+jhhyf@users.noreply.github.com>
2017-08-04 16:25:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let links = ["https://vignette2.wikia.nocookie.net/diepio/images/d/d6/Tanksksksksk.png/revision/latest?cb=20160906115151",
+var links = ["https://vignette2.wikia.nocookie.net/diepio/images/d/d6/Tanksksksksk.png/revision/latest?cb=20160906115151",
 
 "https://media.mojang.com/blog-image/0db18353862a2f2d4d029b757914935a930311a4/0_17_Update_Mojang_Blog_1024x576.png",
 
@@ -6,22 +6,22 @@ let links = ["https://vignette2.wikia.nocookie.net/diepio/images/d/d6/Tanksksksk
 
 ];
 
-let titles = ["Diep.io level 0 tank", "Minecraft end dragon", "Minecraft world",];
-let index = 0;
-let autoplay = true;
+var titles = ["Diep.io level 0 tank", "Minecraft end dragon", "Minecraft world",];
+var index = 0;
+var autoplay = true;
 
 function prevPic() {
-  let img = document.getElementById("pic");
+  var img = document.getElementById("pic");
   index--;
   img.src = links[index % links.length];
 }
 function nextPic() {
   //console.log("nextPic: " + index);
-  let img = document.getElementById("pic");
+  var img = document.getElementById("pic");
   index++;
   img.src = links[index % links.length];
   
-  let title = document.getElementById("title");
+  var title = document.getElementById("title");
   title.innerHTML = titles[index % links.length];
   //console.log(title);
   
